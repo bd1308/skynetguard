37e510305aed4285a133f64bfbb985111a8246ef
Britt Dodd <brittman914@gmail.com>
2024-10-16 17:23:30 -0400

    updates

---
+++ renderer.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+file = open('skynetfilter','r')
+domains = file.readlines()
+file.close()
+
+def sort_domains(s):
+    return list(s.split('.')[-1])
+domains_sorted = sorted(domains, key=sort_domains)
+print(domains_sorted)
+
+output = open('docs/skynetallow', 'w+')
+for line in domains_sorted:
+    output.write(f"@@||{line.strip()}^$client=skynet,important\n")
+
+output.write("#Default block\n")
+output.write("||*.*^$client=skynet\n")
+output.close()
