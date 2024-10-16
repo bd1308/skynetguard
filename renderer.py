00c33f782aab7768412138a41890969fdec0dba5
Britt Dodd <brittman914@gmail.com>
2024-10-16 18:19:25 -0400

    update

--- renderer.py
+++ renderer.py
@@ -3,6 +3,10 @@ file = open('skynetfilter','r')
 domains = file.readlines()
 file.close()
 
+file2 = open('skynetredirects','r')
+redirects = file2.readlines()
+file.close()
+
 def sort_domains(s):
     return list(s.split('.')[-1])
 domains_sorted = sorted(domains, key=sort_domains)
@@ -12,6 +16,9 @@ output = open('tmp_skynetallow', 'w+')
 for line in domains_sorted:
     output.write(f"@@||{line.strip()}^$client=skynet,important\n")
 
+for line in redirects:
+    output.write(f"@@||{line.split(',')[0]}^$dnsrewrite=NOERROR;CNAME;{line.split(',')[1]},client=skynet,important")
+    
 output.write("#Default block\n")
 output.write("||*.*^$client=skynet\n")
 output.close()
