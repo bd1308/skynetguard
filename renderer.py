d944c4b19d03997f8c5417d7197dc5af8a62ded0
Britt Dodd <brittman914@gmail.com>
2024-10-17 13:55:45 -0400

    skip commented out lines, get rid of youtube

--- renderer.py
+++ renderer.py
@@ -14,10 +14,12 @@ print(domains_sorted)
 
 output = open('tmp_skynetallow', 'w+')
 for line in domains_sorted:
-    output.write(f"@@||{line.strip()}^$client=skynet,important\n")
+    if '#' not in line:
+        output.write(f"@@||{line.strip()}^$client=skynet,important\n")
 
 for line in redirects:
-    output.write(f"@@||{line.split(',')[0]}^$dnsrewrite=NOERROR;CNAME;{line.split(',')[1]},client=skynet,important\n")
+    if '#' not in line:
+        output.write(f"@@||{line.split(',')[0]}^$dnsrewrite=NOERROR;CNAME;{line.split(',')[1]},client=skynet,important\n")
 
 output.write("#Default block\n")
 output.write("||*.*^$client=skynet\n")
